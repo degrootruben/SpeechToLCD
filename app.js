d71d79b1cdeeca71c2888cdc8a5664034e86a2e9
Ruben de Groot <rpg.degroot@gmail.com>
2021-02-06 16:28:04 +0100

    Changed webserver to https for use of SpeechRecognition

--- app.js
+++ app.js
@@ -1,31 +1,17 @@
 const express = require("express");
-const formidable = require("formidable");
-const LCD = require("lcd");
-const lcd = new LCD({rs: 7, e: 11, data: [10, 15, 16, 18], cols: 16, rows: 2});
+const fs = require("fs");
+const https = require("https");
 
 const app = express();
+const key = fs.readFileSync("./key.pem");
+const cert = fs.readFileSync("./cert.pem");
+const server = https.createServer({key: key, cert: cert}, app);
 
 app.use(express.static("public", { root: __dirname }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-lcd.on('ready',async _ => {
-    lcd.setCursor(0, 0);
-    lcd.autoscroll();
-    lcd.print("Hello world!", err => {
-    	if (err) {
-    		throw err;
-    	}
-	lcd.close();
-    });
-    await new Promise(resolve => setTimeout(resolve, 5000));
-    console.log("Waiting is over!");
-    lcd.clear()
-    lcd.setCursor(0,0);
-    lcd.blink();
-});
-
-app.listen(8000, () => {
+server.listen(8000, () => {
     console.log("Listening on port 8000!");
 });
 
@@ -34,24 +20,11 @@ app.get("/", (req, res) => {
 });
 
 app.post("/upload_audio", (req, res) => {
-    new formidable.IncomingForm().parse(req, (err, fields, files) => {
-        if (err) {
-          console.error('Error', err);
-          throw err;
-        }
-        console.log('Fields', fields);
-        console.log('Files', files);
-        for (const file of Object.entries(files)) {
-          console.log(file);
-        }
-    });
+
 });
 
 app.use((req, res) => {
     res.status(404).send("Error 404 not found");
 });
 
-process.on('SIGINT', _ => {
-    lcd.close();
-    process.exit();
-});
+process.on("SIGINT", _ => process.exit());
