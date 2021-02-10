be35b88fa2c7b2ee3b56aaa8de99ebfc14982a37
Ruben de Groot <rpg.degroot@gmail.com>
2021-02-10 20:01:30 +0100

    Using socket.io to now send the audio to the server

--- app.js
+++ app.js
@@ -6,9 +6,16 @@ const https = require("https");
 const app = express();
 const key = fs.readFileSync("./key.pem");
 const cert = fs.readFileSync("./cert.pem");
-const server = https.createServer({key: key, cert: cert}, app);
+const server = https.createServer({ key: key, cert: cert }, app);
+const io = require("socket.io")(server, {
+    cors: {
+      origin: "http://localhost:8000",
+    },
+  });
 
 const upload = multer({ dest: "./public/uploads/" });
+
+require("dotenv").config();
 
 app.use(express.static("public", { root: __dirname }));
 app.use(express.urlencoded({ extended: true }));
@@ -22,14 +29,16 @@ app.get("/", (req, res) => {
     res.status(200).sendFile("./index.html");
 });
 
-app.post("/upload_audio", upload.single("audioBlob"), (req, res, next) => {
-    let uploadLocation = __dirname + "\\public\\uploads\\" + req.file.originalname;
-    fs.writeFileSync(uploadLocation, Buffer.from(new Uint8Array(req.file.buffer)));
-    res.sendStatus(200);
-});
-
 app.use((req, res) => {
     res.status(404).send("Error 404 not found");
+});
+
+io.on('connect', (client) => {
+    client.on('message', async function (data) {
+        const dataURL = data.audio.dataURL.split(',').pop();
+        let fileBuffer = Buffer.from(dataURL, 'base64');
+        fs.writeFileSync("./upload.wav", fileBuffer);
+    });
 });
 
 process.on("SIGINT", _ => process.exit());
